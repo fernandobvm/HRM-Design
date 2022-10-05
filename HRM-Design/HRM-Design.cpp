6336addebad5962a10bf8bc269200240255894cd
Fernando Mendes <fernandobvm@gmail.com>
2022-10-05 17:38:32 -0300

    adicionando os calculos envolvendo a garganta, as massas de propelente e os parâmetro de injeção

--- HRM-Design/HRM-Design.cpp
+++ HRM-Design/HRM-Design.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+#define PI 3.141592653589793
 
 using namespace std;
 
@@ -24,6 +27,9 @@ public:
     void readPropellant();
 
     double calculatePressure(double _temperature);
+
+    double getFuelDensity();
+    double getOxidizerDensity();
 
 };
 
@@ -75,6 +81,14 @@ double Propellant::calculatePressure(double _temperature) {
     return C * _temperature * _temperature + B * _temperature + A;
 }
 
+double Propellant::getFuelDensity() {
+    return fuel_density;
+}
+
+double Propellant::getOxidizerDensity(){
+    return oxidizer_density;
+}
+
 class Ambient {
 protected:
     double ambient_temperature;
@@ -102,20 +116,31 @@ class Injector {
 protected:
     string type;
     double discharge_coefficient;
+    double orifice_diameter;
+    double injection_area;
+    double orifice_number;
 public:
 
     Injector();
     ~Injector();
 
-    void setInjector(string _type, double _discharge_coefficient);
+    void setInjector(string _type, double _discharge_coefficient, double _orifice_diameter);
+    void calculateInjector( double _dot_m_oxidizer, double _delta_pressure, double _oxidizer_density);
 };
 
 Injector::Injector() {}
 Injector::~Injector() {}
 
-void Injector::setInjector(string _type, double _discharge_coefficient) {
+void Injector::setInjector(string _type, double _discharge_coefficient, double _orifice_diameter) {
     type = _type;
     discharge_coefficient = -discharge_coefficient;
+    orifice_diameter = _orifice_diameter;
+}
+
+void Injector::calculateInjector(double _dot_m_oxidizer, double _delta_pressure, double _oxidizer_density ){
+    injection_area = _dot_m_oxidizer / (discharge_coefficient * sqrt(2 * _oxidizer_density * _delta_pressure));
+    orifice_number = 4 * injection_area / (PI * pow(orifice_diameter, 2));
+
 }
 
 class Motor {
@@ -137,11 +162,27 @@ protected:
     Propellant propellant;
     Injector injector;
 
+    double throat_area;
+    double throat_diameter;
+    double exit_area;
+
+    //Total values
+    double dot_m_propellant;
+    double dot_m_fuel;
+    double dot_m_oxidizer;
+
+    double oxidizer_mass;
+    double fuel_mass;
+    double propellant_mass;
+
+    void calculateParameters();
+
 public:
     Motor();
     ~Motor();
 
     void setParameters(double _thrust, double _chamber_pressure, double _of, double _cf, double _c_star, double _ef_c_star, double _area_ratio, double _Isp, double _burning_time, double _n_port, double _initial_port_diameter, Ambient _ambient, Propellant _propellant, Injector _injector);
+    double getThroatArea();
 };
 
 Motor::Motor() {}
@@ -164,8 +205,31 @@ void Motor::setParameters(double _thrust, double _chamber_pressure, double _of, 
     propellant = _propellant;
     injector = _injector;
     tank_pressure = _propellant.calculatePressure(_ambient.getTemperature());
+
+    Motor::calculateParameters();
 }
 
+void Motor::calculateParameters(){
+    throat_area = thrust / (chamber_pressure * cf);
+    throat_diameter = sqrt  (4 * throat_area / PI);
+    exit_area = area_ratio * throat_area;
+
+    dot_m_propellant = chamber_pressure * throat_area / cf;
+    dot_m_fuel = dot_m_propellant / (of + 1);
+    dot_m_oxidizer = dot_m_propellant - dot_m_fuel;
+
+    oxidizer_mass = dot_m_oxidizer * burning_time;
+    fuel_mass = dot_m_fuel * burning_time;
+    propellant_mass = oxidizer_mass + fuel_mass;
+
+    injector.calculateInjector(dot_m_oxidizer, tank_pressure - chamber_pressure, propellant.getOxidizerDensity());
+
+    //TODO: Calculate tank dimensions
+}
+
+double Motor::getThroatArea() {
+    return throat_area;
+}
 
 class Simulation {
 protected:
@@ -201,8 +265,10 @@ int main()
     Simulation simulacao;
     
     ambiente.setTemperature(25.0);
-    injetor.setInjector("Default", 0.4);
+    injetor.setInjector("Default", 0.4, 2.6 / 1000);
     propelente.setPropellant("Paraffin", 920.0, "N2O", 720.0, 0.467, 0.543, 3039240, 79752.817, 773.269);
     motor.setParameters(1500, 3000000, 5, 1.49, 1578.84102, 0.8, 5.0, 235.17805, 5.0, 1, 0.03, ambiente, propelente, injetor);
     simulacao.setSimulation(0.01, motor);
+
+    cout << "Throat Area (m2): " << motor.getThroatArea();
 }
